c586176dfbdfb758ab98aac96350607a45796ccc
ginkenta <48866280+ginkenta@users.noreply.github.com>
2021-06-22 20:31:11 +0300

    feat: added func to create token and add it to expired

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 import { compare, genSalt, hash } from 'bcrypt';
 import { User } from '../../models/user.entity';
 import { UserService } from '../user/user.service';
+import { expiredTokens } from '../../middlewares/is.token.availible.middleware';
+
+// export const expiredTokens = new Set();
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
 
   async createPassword(pass: string): Promise<string> {
     const salt = await genSalt(8);
@@ -28,5 +37,17 @@ export class AuthService {
       return otherInfo;
     }
     return null;
+  }
+
+  async addExpiredToken(token: string): Promise<boolean> {
+    expiredTokens.add(token);
+    return true;
+  }
+
+  async createToken(user: User): Promise<any> {
+    const payload = { id: user.id };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
   }
 }
