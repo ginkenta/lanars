276b3ddd4159f065675fbbc73a00c5ac8c7ab1d6
ginkenta <48866280+ginkenta@users.noreply.github.com>
2021-06-22 17:02:26 +0300

    feat: added signup

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,18 +7,30 @@ import {
   UseGuards,
   Req,
   Request,
+  Body,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { UserSignupInterface } from 'src/interfaces/user/signup';
+import { UserService } from '../user/user.service';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Req() req: Request) {
     // @ts-ignore
     return req.user;
+  }
+
+  @Post('signup')
+  async signup(@Body() body: UserSignupInterface) {
+    const password = await this.authService.createPassword(body.password);
+    return await this.userService.createNewUser({ ...body, password });
   }
 }
