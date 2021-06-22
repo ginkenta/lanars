bf6fc8e08325296518a17b1f60d381099bf91fca
ginkenta <48866280+ginkenta@users.noreply.github.com>
2021-06-22 16:18:28 +0300

    feat: added empty portfolio module

---
+++ src/modules/portfolio/portfolio.controller.ts
@@ -0,0 +1,12 @@
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { configService } from 'src/config/config.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig())],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
